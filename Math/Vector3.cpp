0d2933c6843d6378c08d968cc2a82d70ef8e4c2c
Skyteks <DanielEmig@gmx.de>
2024-02-28 20:37:48 +0100

    Add basic math functions

--- Math/Vector3.cpp
+++ Math/Vector3.cpp
@@ -1,2 +1,43 @@
-
+#include "pch.h"
 #include "Vector3.h"
+#include <math.h>
+
+Vector3::Vector3()
+	: x(0.0f), y(0.0f), z(0.0f)
+{
+}
+
+Vector3::Vector3(float _x, float _y, float _z)
+	: x(_x), y(_y), z(_z)
+{
+}
+
+float Vector3::Magnitude() const
+{
+	return sqrtf(x * x + y * y + z * z);
+}
+
+float Vector3::SqrMagnitude() const
+{
+	return x * x + y * y + z * z;
+}
+
+float Vector3::Dot(Vector3& a, Vector3& b)
+{
+	return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+Vector3 Vector3::operator+(Vector3& other)
+{
+	return Vector3(x + other.x, y + other.y, z + other.z);
+}
+
+Vector3 Vector3::operator-(Vector3& other)
+{
+	return Vector3(x - other.x, y - other.y, z - other.z);
+}
+
+Vector3 Vector3::operator*(float scalar)
+{
+	return Vector3(x * scalar, y * scalar, z * scalar);
+}
--- Math/Vector3.h
+++ Math/Vector3.h
@@ -2,4 +2,50 @@
 struct Vector3
 {
 	float x, y, z;
+
+	Vector3();
+
+	Vector3(float _x, float _y, float _z = 0.0f);
+
+	/// <summary>
+	/// The lenght of the vector
+	/// </summary>
+	/// <returns></returns>
+	float Magnitude() const;
+
+	/// <summary>
+	/// The squared lenght of the vector
+	/// </summary>
+	/// <returns></returns>
+	float SqrMagnitude() const;
+
+	/// <summary>
+	/// The dot product of two vectors
+	/// </summary>
+	/// <param name="a"></param>
+	/// <param name="b"></param>
+	/// <returns></returns>
+	static float Dot(Vector3& a, Vector3& b);
+
+	/// <summary>
+	/// Adding two vectors
+	/// </summary>
+	/// <param name="other"></param>
+	/// <returns></returns>
+	Vector3 operator+(Vector3& other);
+
+	/// <summary>
+	/// Subtracting a vector from another
+	/// </summary>
+	/// <param name="other"></param>
+	/// <returns></returns>
+	Vector3 operator-(Vector3& other);
+
+	/// <summary>
+	/// Scaling a vector
+	/// </summary>
+	/// <param name="scalar"></param>
+	/// <returns></returns>
+	Vector3 operator*(float scalar);
+
 };
